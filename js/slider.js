f0f12002c95eb594620c182d6d1ed17752b48224
Yevheniia Khvorostiana <yevheniia.khvorostiana@gmail.com>
2020-01-25 21:19:31 +0200

    edits

--- js/slider.js
+++ js/slider.js
@@ -4,6 +4,6 @@ $('.js-slider').slick({
   speed: 300,
   slidesToShow: 1,
   adaptiveHeight: true,
-  prevArrow: '<button class="slider-arrow slider-arrow--prev"></button><div class="slider-left"></div></button>',
-  nextArrow: '<button class="slider-arrow slider-arrow--next"></button><div class="slider-right"></div></button>'
+  prevArrow: '<button class="slider-arrow slider-arrow--prev"><div class="slider-left"></div></button>',
+  nextArrow: '<button class="slider-arrow slider-arrow--next"><div class="slider-right"></div></button>'
 });
